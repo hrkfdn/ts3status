009fdac1cc6941aaf465ebdeefe3098ea35e31ca
Henrik Friedrichsen <henrik@affekt.org>
2014-05-10 16:13:40 +0200

    import ts3status

---
+++ config.py
@@ -0,0 +1,15 @@
+# serverquery hostname
+hostname = "host"
+
+# serverquery port
+port = 10011
+
+# serverquery username
+username = "user"
+
+# serverquery password
+password = "pass"
+
+# host to be displayed on the webpage
+# this will also be used for the connect button
+displayhost = "example.com"
---
+++ ts3status.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import config
+
+from ts3query import TS3Connection
+from flask import Flask, render_template, Markup
+app = Flask(__name__)
+
+t = TS3Connection(config.hostname, config.port)
+
+def connect():
+    t.connect()
+    t.login(config.username, config.password)
+    t.sendcmd("use sid=1")
+
+def disconnect():
+    t.disconnect()
+
+def is_empty(clients):
+    if len(clients) == 0:
+        return True
+
+    # we don't consider serverquery clients (such as ourself) as
+    # "real" clients
+    for c in clients:
+        if c["client_type"] == 0:
+            return False
+    return True
+
+def channel_html(chan, clientsinchannel):
+    if chan["cid"] == 0 or is_empty(clientsinchannel):
+        result = "<li class=\"chan_empty\">" + str(chan["channel_name"])
+    else:
+        result = "<li class=\"chan_populated\">" + str(chan["channel_name"])
+    return result + "</li>\n"
+
+def client_html(client):
+    classes = "client"
+    classes += " client_inputmute" if client["client_input_muted"] else ""
+    classes += " client_outputmute" if client["client_output_muted"] else ""
+    classes += " client_away" if client["client_away"] == 1 else ""
+    return "<li class=\"" + classes + "\">" + client["client_nickname"] + "</li>\n"
+
+def generate_overview(ctree, clients):
+    result = "<ul>\n"
+
+    for channel in ctree:
+        inchan = clients.listinchannel(channel["cid"])
+
+        result += channel_html(channel, inchan)
+
+        if len(channel["children"]) > 0:
+            result += "<ul>\n"
+        if len(inchan) > 0:
+            result += "<ul>\n"
+            for client in inchan:
+                if client["client_type"] == 0: # only show real clients
+                    result += client_html(client)
+            result += "</ul>\n"
+
+
+        result += generate_overview(channel["children"], clients)
+
+        if len(channel["children"]) > 0:
+            result += "</ul>\n"
+        result += "</ul>\n"
+    return result
+
+@app.route("/")
+def main():
+    connect()
+
+    channels = t.getchannels()
+    clients = t.getclients("-away -voice -info")
+    info = t.getserverinfo()
+
+    disconnect()
+
+    out = generate_overview(channels.chanlist[0]["children"], clients)
+
+    return render_template("index.html", 
+            server_title=info["virtualserver_name"],
+            server_platform=info["virtualserver_platform"],
+            server_version=info["virtualserver_version"],
+            server_host=config.hostname,
+            display_host=config.displayhost,
+            overview=Markup(out))
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
